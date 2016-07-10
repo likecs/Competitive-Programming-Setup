e7cdade1acb43e9ba839c2593970297d5d4e1a60
likecs <f2014028@pilani.bits-pilani.ac.in>
2016-07-10 17:25:40 +0530

    Added better checking functionality

--- output_checker.cpp
+++ output_checker.cpp
@@ -1,7 +1,3 @@
-//Below is used to compare the output of two files
-//The "out.txt" file is generated from the "work.cpp" file
-//The "res.txt" file is generated from the "check.cpp" file
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,11 +10,11 @@ int main() {
 	output2.open("res.txt", ios::in);
 
 	if (!output1) {
-		cout<<"Can't open output file 1\n";
+		cerr<<"Can't open output file 1\n";
 		return 0;
 	}
 	if (!output2) {
-		cout<<"Can't open output file 2\n";
+		cerr<<"Can't open output file 2\n";
 		return 0;	
 	}
 
@@ -26,14 +22,33 @@ int main() {
 	string str1, str2;
 	while (getline(output1, str1)) {
 		getline(output2, str2);
-		if (str1 != str2) {
-			cout<<"Output differs at line number : "<<lines<<"\n";
+		int a = str1.length();
+		int b = str2.length();
+		int lim = min(a, b);
+		int pos = 0;
+		bool c = false;
+		for(int i=0; i<lim; ++i) {
+			if (str1[i] != str2[i]) {
+				c = true;
+				break;
+			}
+			pos += 1;
+		}
+		if (c || a!=b) {
+			cerr<<"Output differs at line number : "<<lines<<"\n";
+			cerr<<"The characters diifer at position : "<<pos+1<<"\n";
+			if (a < b) {
+				cerr<<"Output in file out.txt has shorter length\n";
+			}
+			else if (b < a) {
+				cerr<<"Output in file res.txt has shorter length\n";		
+			}
 			return 0;
 		}
 		lines += 1;
 	}
-	cout<<"Scanned a total of "<<lines<<" lines from both files\n";
-	cout<<"Both output files are same\n";
+	cerr<<"Scanned a total of "<<lines<<" lines from both files\n";
+	cerr<<"Both output files are same\n";
 	output1.close();
 	output2.close();
 	return 0;
