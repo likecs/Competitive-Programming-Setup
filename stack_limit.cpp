a34a6788e3a6ecb365732c7953ea04cffb1b21fc
likecs <f2014028@pilani.bits-pilani.ac.in>
2017-01-05 20:56:45 +0530

    Updates

--- stack_limit.cpp
+++ stack_limit.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main() {
 	const rlim_t Stack_size = 32L * 1024 * 1024;
-    struct rlimit lim = {Stack_size, Stack_size};
-    if(setrlimit(RLIMIT_STACK, &lim) == -1) assert(false);
-    
+	struct rlimit lim = {Stack_size, Stack_size};
+	if(setrlimit(RLIMIT_STACK, &lim) == -1) assert(false);
+	
 	return 0;
 }
